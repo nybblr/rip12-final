97e07ee01b15324872776a10ef2986736953ebd2
Leo Keselman <Chipper10@gmail.com>
2012-12-04 23:11:23 -0500

    Tool Use Tab Added
    Currently exact same as tabRipPlanner

--- tabRipPlannerApp.cpp
+++ tabRipPlannerApp.cpp
@@ -8,6 +8,7 @@
 
 #include "GRIPApp.h"
 #include "tabRipPlanner.h"
+#include "toolUsePlanner.h"
 
 #ifndef _WIN32
 #include "JacobianMouseTab.h"
@@ -21,6 +22,7 @@ extern wxNotebook* tabView;
 class RipPlannerTabApp : public GRIPApp {
 	virtual void AddTabs() {
 		tabView->AddPage(new RipPlannerTab(tabView), wxT("RIP Planner"));
+		tabView->AddPage(new ToolUsePlannerTab(tabView), wxT("Tool Use"));
 #ifndef _WIN32
 		tabView->AddPage(new JacobianMouseTab(tabView), wxT("Mouse Planner"));
 #endif
